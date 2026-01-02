d61ae6a61b4ff152ec2a1753026c5797a20eb010
rahul <rahulpdy.pandey@gmail.com>
2026-01-02 12:08:08 +0530

    commit-2

--- src/hooks/useScrollDirection.js
+++ src/hooks/useScrollDirection.js
@@ -1,19 +1,30 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function useScrollDirection() {
   const [isScrollingUp, setIsScrollingUp] = useState(true);
+  const lastY = useRef(0);
+  const ticking = useRef(false);
 
   useEffect(() => {
-    let lastY = window.scrollY;
-
+    if (typeof window === 'undefined') return;
+    lastY.current = Math.floor(window.scrollY);
     const onScroll = () => {
-      const currentY = window.scrollY;
-      setIsScrollingUp(currentY < lastY);
-      lastY = currentY;
+      if (!ticking.current) {
+        window.requestAnimationFrame(() => {
+          const currentY = Math.floor(window.scrollY); 
+          if (Math.abs(currentY - lastY.current) > 5) {
+            setIsScrollingUp(currentY < lastY.current);
+            lastY.current = currentY;
+          }
+          
+          ticking.current = false;
+        });
+        ticking.current = true;
+      }
     };
 
-    window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
